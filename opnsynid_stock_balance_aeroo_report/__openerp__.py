9e4a5942645018813ef3867d36c51e65e8c1ec3a
Andhitia Rama <andhitia.r@gmail.com>
2016-10-29 22:11:18 +0700

    [ADD][8.0] opnsynid_stock_balance_aeroo_report

---
+++ opnsynid_stock_balance_aeroo_report/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Balance Aeroo Report",
+    "version": "8.0.1.0.0",
+    "category": "Stock",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "Andhitia Rama, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "opnsynid_stock_card_aeroo_report",
+    ],
+    "data": [
+        "reports/stock_balance_reports.xml",
+        "wizards/print_stock_balance_views.xml",
+    ],
+}
